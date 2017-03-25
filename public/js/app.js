8e3aaa82a89ae12b8dd4200db2024403af7fa2ee
Saleh Suboh <salehsuboh@gmail.com>
2017-03-24 20:24:15 -0400

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,22 @@
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
 var socket = io();
+
+//console.log(name + ' wants to join ' + room);
+
 socket.on('connect', function () {
 	console.log('Connected to the socket.io server');
 });
 
 socket.on('message', function (message) {
-	var momentTimestap = moment.utc(message.timestap);
+	var momentTimestap = moment.utc(message.timestamp);
+	var $message = jQuery('.messages');
+
 	console.log('New message:');
 	console.log(message.text);
 
-	jQuery('.messages').append('<p><strong>' + momentTimestap.format('h:mm a') + '</strong> ' + message.text + '</p>');
-
+	$message.append('<p><strong>' + message.name + ' ' + momentTimestap.local().format('h:mm a') + '</strong></p>');
+	$message.append('<p>' + message.text + '</p>');
 });
 
 // handles submitting of new message
@@ -21,8 +28,9 @@ $form.on('submit', function (event) {
 	var $message = $form.find('input[name=message]');
 
 	socket.emit('message', {
+		name: name,
 		text: $message.val()
 	});
 
 	$message.val('');
-});
+}); 
--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 var PORT = process.env.PORT || 3000;
+var moment = require('moment');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app); //start a new server and use express app as boiler
 var io = require('socket.io')(http);
-var moment = require('moment');
-var now = moment();
 
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function (socket) { //socket: some individual connection
 	console.log('User connected via socket.io!');
 
-
 	socket.on('message', function (message) {
 		console.log('Message received: ' + message.text);
 
-		message.timestamp = now.valueOf();
+		message.timestamp = moment().valueOf();
 		//socket.broadcast.emit('message', message); //sends the msg to everyone but myself (my browser)
 		io.emit('message', message); //sends the msg to everyone and every browser in chat application
 	});
 
 	socket.emit('message', {
+		name: 'System',
 		text: 'Welcome to the chat application!',
-		timestamp: now.valueOf()
+		timestamp: moment().valueOf()
 	});
 }); //listen for events
   
